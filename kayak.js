5e6a0302313e3965973dd7608544e3614fd86f71
Guy Marom <guy.marom@sparkbeyond.com>
2021-02-04 08:05:28 +0200

    Kayak URLs

---
+++ kayak.js
@@ -0,0 +1,25 @@
+const departureDates = ["2021-02-22", "2021-03-01", "2021-03-08"]
+const airports = ["SYD", "PER", "MEL", "ADL"]
+
+const root = "https://www.kayak.com/flights/TLV-"
+const suffix = "1adults/children-11?sort=bestflight_a&fs=airlines=-MULT"
+const roundTemplate = `${root}$AIRPORT/$DEPDATE-flexible/2021-03-31-flexible/${suffix}`
+const onewayTemplate = `${root}$AIRPORT/$DEPDATE-flexible/${suffix}`
+
+function templateToUrl(template, airport, depDate) {
+    return template.replace("$AIRPORT", airport).replace("$DEPDATE", depDate);
+}
+
+function act(url) {
+    console.log(url);
+}
+
+departureDates.forEach(depDate => {
+   airports.forEach(airport => {
+       let oneWayUrl = onewayTemplate.replace("$AIRPORT", airport).replace("$DEPDATE", depDate);
+       act(oneWayUrl);
+
+       let roundUrl = roundTemplate.replace("$AIRPORT", airport).replace("$DEPDATE", depDate);
+       act(roundUrl)
+   });
+});
